7a636e6b4c51701da17d02c519e86379fb431f46
Oran Bar Natan <39872210+Oranba@users.noreply.github.com>
2025-10-22 15:10:07 +0300

    Add files via upload
    initial commit

---
+++ springSecurityDemoApp/src/test/java/com/spring_security/springSecurityDemoApp/SpringSecurityDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_security.springSecurityDemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
